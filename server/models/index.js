cf8d351befcb1a3890e38c4ed89ee802244a55a7
coterone <coteroneric@gmail.com>
2023-12-04 18:25:42 -0500

    removed modules & package-lock.json... redownloaded it because of error on render.

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,8 @@
 const User = require('./User');
+// Need to change THOUGHT to BlogPost
 const Thought = require('./Thought');
 
 module.exports = { User, Thought };
+
+
+// blog post , post , comment 
